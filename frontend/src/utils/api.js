caa384d6daa6a403a876aaa10c794adec409773f
irmo12 <omriws@hushmail.com>
2023-03-25 12:33:10 +0300

    update url for api requests

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -77,7 +77,7 @@ class Api {
 }
 
 const api = new Api({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'api.omribt.chickenkiller.com',
   headers: {
     'Content-Type': 'application/json',
   },
